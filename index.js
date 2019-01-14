044a3e7e934f2024849c81a5cb1e0893de61b595
Freddie Ridell <contact@freddieridell.com>
2019-01-14 16:43:55 +0000

    added better rapper name generation

--- index.js
+++ index.js
@@ -36,21 +36,38 @@ const rge = (...args) => {
 	}
 }
 
+
 const generator = rge({
-	root: ({ unaugmentedRoot,  maffia, self, }) => [
-		rge`The ${maffia}: ${unaugmentedRoot}`,
-		rge`${unaugmentedRoot} ${self}`,
+	root: ({ root, bigSean, prefixHonorific,suffixHonorifix, role}) => [
+		rge`${prefixHonorific} ${bigSean}`,
+		rge`${bigSean}`,
+		rge`${bigSean}-${suffixHonorifix}`,
+		rge`The ${role}`,
 	],
-	unaugmentedRoot: ({ honorific, adjective, noun,}) => [
-		rge`${honorific} ${adjective} ${noun}`,
+	prefixHonorific: () => [ "Mr.", "Mrs.", "Miss", "Ms.", "Mx.", "Sir.", "Dr.", "Lady", "Lord" ],
+	suffixHonorifix: () => [ "samma", "senpai", "dono" ],
+
+	bigSean: ({ simpleAdjective, identity }) => [rge`${simpleAdjective} ${identity}`],
+
+	simpleAdjective: ({ size, simpleValueJudgement, age }) => [ size, simpleValueJudgement, age ],
+	simpleValueJudgement: () => [ "Bad", "Nasty", "Stinky", "Smooth", "Wild", "Incomprehensible" ],
+	size: () => [ "Lil'", "Big", "Short", "Fat", "Phat", "Large", "Wide", ],
+	age: () => [ "Young", "Old", "Middle Aged", ],
+
+	identity: () => [ "Stinker", "Nasty", "Paul", "Ibuprophen", "Paracetamol", "Lemsip", "Ferari", "Stinker", "Boi", "Boy", "Shorty","Fatty", "Man", "Woman",],
+
+	role: ({ subject, position, }) => [
+		rge`${position} of ${subject}`,
+		rge`${subject} ${position}`
 	],
-	self: () => [ "themself", "herself", "himself" ],
-	maffia: ({ adjective }) => [ "don", "godfather", rge`${adjective} boss` ],
-	honorific: () => [ "Mr.", "Ms.", "Dr.", ],
-	adjective: () => [ "lil'", "big", "smooth", ],
-	noun: () => [ "ibuprophen", "paracetamol", "lemsip", "ferari"],
+
+	subject: () => ["Piss", "Money", "$$$", "Dollas", "Gold Bars", "Rhyming Couplets", "Shit Talking", "Fire Beats", "Doing Big Shits", "Being Good At Rapping"],
+	position: ({ simpleAdjective, position }) => [
+		...new Array(3).fill(rge`${simpleAdjective} ${position}`),
+		"Ninja", "Doctor", "MC", "Duke", "Master", "Lord", "Don", "Ring Master", "Madame",
+	],
 })
 
-for (const x in Array.from({ length: 3 })) {
+for (const x in Array.from({ length: 10 })) {
 	console.log(generator.root())
 }
